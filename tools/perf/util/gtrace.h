cfc77c995365cabc700bd2033983243e11d39bad
Mayuresh Chitale <mayuresh.chitale@oss.qualcomm.com>
2026-04-29 18:21:33 +0530

    perf tools: Add RISC-V trace PMU record capabilities
    Introduce the required auxiliary API functions allowing the perf core
    to interact with gtrace perf driver. On RISC-V systems the gtrace
    PMU uses the RISC-V E-Trace components and optionally an ATB Bridge
    component for connecting to ARM-side sinks, if it is present on the SoC.
    
    Co-developed-by: Anup Patel <anup.patel@oss.qualcomm.com>
    Signed-off-by: Anup Patel <anup.patel@oss.qualcomm.com>
    Signed-off-by: Mayuresh Chitale <mayuresh.chitale@oss.qualcomm.com>
    Reviewed-by: Nutty Liu <nutty.liu@hotmail.com>
    Reviewed-by: Adrian Hunter <adrian.hunter@intel.com>

---
+++ tools/perf/util/gtrace.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2026 Qualcomm Technologies, Inc.
+ */
+
+#ifndef INCLUDE__UTIL_PERF_GTRACE_H__
+#define INCLUDE__UTIL_PERF_GTRACE_H__
+
+#define GTRACE_AUXTRACE_PRIV_SIZE      sizeof(u64)
+
+#endif
+
